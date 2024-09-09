efe19cd9c99720a6c641b123c6d22989e1f268a8
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-09 22:08:45 +0900

    phx.new upload_experiment --no-ecto

---
+++ lib/upload_experiment_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule UploadExperimentWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use UploadExperimentWeb, :controller` and
+  `use UploadExperimentWeb, :live_view`.
+  """
+  use UploadExperimentWeb, :html
+
+  embed_templates "layouts/*"
+end
